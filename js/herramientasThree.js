4cd55aaaa11e9ae6d233a1251ac57c226a4a0fc4
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-14 17:15:44 -0300

    se agregarom herramientas a la seccion de herramientas

---
+++ js/herramientasThree.js
@@ -0,0 +1,36 @@
+import {herramientasT} from '../exports/data.js'
+const containerCard = document.querySelector('#container__cards');
+
+const her = herramientasT[0].stanley
+
+let fragment = document.createDocumentFragment();
+
+her.forEach( elemet => {
+    const contenedorCard = document.createElement('div');
+    contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3';  
+
+    const card = document.createElement('div');
+    card.classList.add('card__product');
+
+    const imgCard = document.createElement('img');
+    imgCard.classList.add('card-img-top');
+    imgCard.src = elemet.img;
+    imgCard.alt = elemet.title
+
+
+    const cardBody = document.createElement('div');
+    card.classList.add('card-body');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.className = 'card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3';
+    cardTitle.textContent = elemet.title
+
+    card.appendChild(imgCard);
+    cardBody.appendChild(cardTitle);
+    card.appendChild(cardBody);
+    contenedorCard.appendChild(card);
+
+    fragment.appendChild(contenedorCard);
+})
+
+containerCard.appendChild(fragment)
