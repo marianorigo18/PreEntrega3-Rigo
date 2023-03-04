19ca604118648f7739a02f2bd52f9cd65dbc0511
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-04 11:13:19 -0300

    fix commit

--- js/lamparas.js
+++ js/lamparas.js
@@ -7,7 +7,7 @@ lamparas.forEach( elemet => {
 
     contenedorCard.innerHTML = `
     <div class="card__product card-body">
-        <a href="#">
+        <a href="${elemet.link}">
         <img class="card-img-top" src="${elemet.img}" alt="${elemet.title}">
         <div>
             <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${elemet.title}</h5>
