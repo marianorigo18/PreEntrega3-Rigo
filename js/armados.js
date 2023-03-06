8405a40a77ae7b9560bb79438fed894d4bbf73c6
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-06 10:55:01 -0300

    fix commit monday

--- js/armados.js
+++ js/armados.js
@@ -1,18 +1,30 @@
-import {armadosBauhaus} from '../exports/data.js'
+getData();
+async function getData(){
+const response = await fetch('https://marianorigo18.github.io/PreEntrega3-Rigo/data/data.json');
+const data = await response.json();
+showData(data.categorias[3].categorias);
+}
 const containerCard = document.querySelector('#container__cards');
 
-armadosBauhaus.forEach( elemet => {
-    const contenedorCard = document.createElement('div');
-    contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3'; 
+function showData(data){
+    data.forEach( elemet => {
+        elemet.categorias.forEach(el=>{
+            const contenedorCard = document.createElement('div');
+            contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3'; 
+        
+            contenedorCard.innerHTML = `
+            <div class="card__product card-body">
+            <a href="${el.link}">
+                <img class="card-img-top" src="${el.img}" alt="${el.title}">
+                <div>
+                    <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${el.title}</h5>
+                </div>
+            </a>
+            </div>
+            `
+            containerCard.appendChild(contenedorCard)
+        })
+    })
+}
 
-    contenedorCard.innerHTML = `
-    <div class="card__product card-body">
-        <img class="card-img-top" src="${elemet.img}" alt="${elemet.title}">
-        <div>
-            <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${elemet.title}</h5>
-        </div>
-    </div>
-    `
-    containerCard.appendChild(contenedorCard)
-})
 
---
+++ js/verona.js
@@ -0,0 +1,27 @@
+getData();
+async function getData(){
+const response = await fetch('https://marianorigo18.github.io/PreEntrega3-Rigo/data/data.json');
+const data = await response.json();
+showData(data.categorias[3].categorias[1].categorias[0].categorias);
+}
+const containerCard = document.querySelector('#container__cards');
+
+function showData(data){
+    data.forEach( elemet => {
+        const contenedorCard = document.createElement('div');
+        contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3'; 
+    
+        contenedorCard.innerHTML = `
+        <div class="card__product card-body">
+        <a href="${elemet.link}">
+            <img class="card-img-top" src="${elemet.img}" alt="${elemet.title}">
+            <div>
+                <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${elemet.title}</h5>
+            </div>
+        </a>
+        </div>
+        `
+        containerCard.appendChild(contenedorCard)
+    })
+}
+
