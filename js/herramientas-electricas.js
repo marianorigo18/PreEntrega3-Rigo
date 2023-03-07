64e74de33c3a7d11036c02402908be01b181b2cc
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-07 08:28:22 -0300

    fix commit

---
+++ js/herramientas-electricas.js
@@ -0,0 +1,25 @@
+getData();
+async function getData(){
+const response = await fetch('https://marianorigo18.github.io/PreEntrega3-Rigo/data/data.json');
+const data = await response.json();
+showData(data.categorias[4].categorias[0].categorias);
+}
+const containerCard = document.querySelector('#container__cards');
+
+function showData(data){
+    data.forEach( elemet => {
+        const contenedorCard = document.createElement('div');
+        contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3'; 
+    
+        contenedorCard.innerHTML = `
+        <div class="card__product card-body">
+            <img class="card-img-top" src="${elemet.img}" alt="${elemet.title}">
+            <div>
+                <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${elemet.title}</h5>
+            </div>
+        </div>
+        `
+        containerCard.appendChild(contenedorCard)
+    })
+    
+}
