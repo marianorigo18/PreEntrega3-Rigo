3d61e648a340a34a2342f948d63af82686a6a2a7
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-17 09:12:23 -0300

    fix commit

---
+++ js/faroles-rectos.js
@@ -0,0 +1,29 @@
+getData();
+async function getData(){
+const response = await fetch('http://127.0.0.1:5501/data/data.json'||'https://marianorigo18.github.io/PreEntrega3-Rigo/data/data.json');
+const data = await response.json();
+showData(data.categorias[0].categorias[1].categorias[2].categorias);
+}
+const containerCard = document.querySelector('#container__cards');
+
+function showData(data){
+    data.forEach( elemet => {
+        const contenedorCard = document.createElement('div');
+        contenedorCard.className = 'col-6 col-sm-6 col-md-4 col-lg-3'; 
+    
+        contenedorCard.innerHTML = `
+        <div class="card__product card-body">
+        <a href="${elemet.link}">
+            <img class="card-img-top" src="${elemet.img}" alt="${elemet.title}">
+            <div>
+                <h5 class="card-title text-center mt-3 text-black-50 text-uppercase fs-6 fw-normal py-3">${elemet.title}</h5>
+            </div>
+        <a>    
+        </div>
+        `
+        containerCard.appendChild(contenedorCard)
+    })
+    
+}
+
+
